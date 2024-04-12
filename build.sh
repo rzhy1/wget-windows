10eabcbe2200345056c5b70284fdc6dc5d4ec6cd
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-04-12 19:16:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -74,9 +74,9 @@ fi
 # build idn2
 # -----------------------------------------------------------------------------
 if [ ! -f "$INSTALL_PATH"/lib/libidn2.a ]; then
-  wget -nc https://ftp.gnu.org/gnu/libidn/libidn2-2.3.3.tar.gz
-  tar -xf libidn2-2.3.3.tar.gz
-  cd libidn2-2.3.3 || exit
+  wget -nc https://ftp.gnu.org/gnu/libidn/libidn2-2.3.2.tar.gz
+  tar -xf libidn2-2.3.2.tar.gz
+  cd libidn2-2.3.2 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
