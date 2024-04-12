cd1b8be04d92a38917e3f0df46009496dc439284
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-04-12 08:20:02 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -74,9 +74,9 @@ fi
 # build idn2
 # -----------------------------------------------------------------------------
 if [ ! -f "$INSTALL_PATH"/lib/libidn2.a ]; then
-  wget -nc https://ftp.gnu.org/gnu/libidn/libidn2-2.3.7.tar.gz
-  tar -xf libidn2-2.3.7.tar.gz
-  cd libidn2-2.3.7 || exit
+  wget -nc https://ftp.gnu.org/gnu/libidn/libidn2-2.3.0.tar.gz
+  tar -xf libidn2-2.3.0.tar.gz
+  cd libidn2-2.3.0 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
@@ -93,9 +93,9 @@ fi
 # build unistring
 # -----------------------------------------------------------------------------
 if [ ! -f "$INSTALL_PATH"/lib/libunistring.a ]; then
-  wget -nc https://ftp.gnu.org/gnu/libunistring/libunistring-1.2.tar.gz
-  tar -xf libunistring-1.2.tar.gz
-  cd libunistring-1.2 || exit
+  wget -nc https://ftp.gnu.org/gnu/libunistring/libunistring-1.1.tar.gz
+  tar -xf libunistring-1.1.tar.gz
+  cd libunistring-1.1 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
