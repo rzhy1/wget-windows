46f23dec4c644e08abeef6edab5f7f97eb2dbddb
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-04-12 18:37:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -362,9 +362,9 @@ fi
 # build openssl
 # -----------------------------------------------------------------------------
 if [ ! -f "$INSTALL_PATH"/lib/libssl.a ]; then
-  wget -nc https://www.openssl.org/source/openssl-3.1.2.tar.gz
-  tar -xf openssl-3.1.2.tar.gz
-  cd openssl-3.1.2 || exit
+  wget -nc https://www.openssl.org/source/openssl-3.1.1.tar.gz
+  tar -xf openssl-3.1.1.tar.gz
+  cd openssl-3.1.1 || exit
   ./Configure \
   --static \
   -static \
