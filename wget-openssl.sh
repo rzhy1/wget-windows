7f6444e0e44a40b397853e7aef1677027dcc88d3
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-05-08 21:20:20 +0800

    Update wget-openssl.sh

--- wget-openssl.sh
+++ wget-openssl.sh
@@ -12,9 +12,9 @@ export MINGW_STRIP_TOOL=x86_64-w64-mingw32-strip
 # build idn2
 # -----------------------------------------------------------------------------
 if [ ! -f "$INSTALL_PATH"/lib/libidn2.a ]; then
-  wget -nc https://ftp.gnu.org/gnu/libidn/libidn2-2.3.0.tar.gz
-  tar -xf libidn2-2.3.0.tar.gz
-  cd libidn2-2.3.0 || exit
+  wget -nc https://github.com/unypkg/libidn2/releases/download/2.3.7-2024-04-28T20.51.37Z/libidn2-2.3.7-source.tar.xz
+  tar -xf libidn2-2.3.7-source.tar.xz
+  cd libidn2-2.3.7 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --enable-static \
