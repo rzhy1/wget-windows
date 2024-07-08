bcbf02f0c0f83231af39449586768062a35499cc
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-07-08 22:43:36 +0800

    Update wget-openssl.sh（libidn2-2.3.7）

--- wget-openssl.sh
+++ wget-openssl.sh
@@ -12,7 +12,7 @@ export MINGW_STRIP_TOOL=x86_64-w64-mingw32-strip
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build libidn2⭐⭐⭐⭐⭐⭐" 
 # -----------------------------------------------------------------------------
 if [ ! -f "$INSTALL_PATH"/lib/libidn2.a ]; then
-  wget -O- https://ftp.gnu.org/gnu/libidn/libidn2-2.3.0.tar.gz | tar xz
+  wget -O- https://ftp.gnu.org/gnu/libidn/libidn2-2.3.7.tar.gz | tar xz
   cd libidn2-* || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
