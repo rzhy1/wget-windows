8d20c9f364f212aa5f4cab554fc387ae75fcd627
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-11-11 21:29:46 +0800

    Update wget-build.sh

--- wget-build.sh
+++ wget-build.sh
@@ -383,7 +383,7 @@ if [[ "$ssl_type" == "gnutls" ]]; then
   #wget -O- https://ftp.gnu.org/gnu/wget/wget-1.24.5.tar.gz | tar xz
   git clone -j$(nproc)  https://gitlab.com/gnuwget/wget.git wget-1.25.0
   cd wget-* || exit 1
-  bash .bootstrap || exit 1
+  bash ./bootstrap --skip-po || exit 1
   chmod +x configure
   CFLAGS="-I$INSTALL_PATH/include -DGNUTLS_INTERNAL_BUILD=1 -DCARES_STATICLIB=1 -DPCRE2_STATIC=1 -DNDEBUG -O3 -pipe -march=$WGET_ARCH -mtune=generic" \
    LDFLAGS="-L$INSTALL_PATH/lib -static -static-libgcc" \
@@ -427,7 +427,7 @@ else
   #wget -O- https://ftp.gnu.org/gnu/wget/wget-1.25.0.tar.gz | tar xz
   git clone -j$(nproc)  https://gitlab.com/gnuwget/wget.git wget-1.25.0
   cd wget-* || exit 1
-  bash .bootstrap || exit 1
+  bash ./bootstrap --skip-po || exit 1
   chmod +x configure
   # cp ../windows-openssl.diff .
   # patch src/openssl.c < windows-openssl.diff
