af6fffd86d5681c272c7b7c65e02b5ef35faa593
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2024-11-15 20:29:24 +0800

    Update wget-build.sh

--- wget-build.sh
+++ wget-build.sh
@@ -356,8 +356,8 @@ fi
 echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build openssl⭐⭐⭐⭐⭐⭐"
 # -----------------------------------------------------------------------------
 if [[ "$ssl_type" == "openssl" ]] && [ ! -f "$INSTALL_PATH"/lib/libssl.a ]; then
-  wget -O- https://github.com/openssl/openssl/releases/download/openssl-3.4.0/openssl-3.4.0.tar.gz | tar xz
-  #wget -O- https://openssl.org/source/old/1.1.1/openssl-1.1.1w.tar.gz | tar xz
+  #wget -O- https://github.com/openssl/openssl/releases/download/openssl-3.4.0/openssl-3.4.0.tar.gz | tar xz
+  wget -O- https://openssl.org/source/old/1.1.1/openssl-1.1.1w.tar.gz | tar xz
   cd openssl-* || exit
   ./Configure \
   -static \
