2a64b2eb5c96cc9a856eba40c4ef247ac313b461
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2025-07-30 23:03:04 +0800

    export CPPFLAGS="$CPPFLAGS -DGNULIB_LIBPOSIX"

--- wget-build.sh
+++ wget-build.sh
@@ -437,8 +437,8 @@ if [[ "$ssl_type" == "gnutls" ]]; then
   rm -rf wget-*
   wget -O- https://ftp.gnu.org/gnu/wget/wget-1.25.0.tar.gz | tar xz
   cd wget-* || exit 1
-  sed -i '/#include <config.h>/a #include <stdlib.h>' lib/openat-die.c
   chmod +x configure
+  export CPPFLAGS="$CPPFLAGS -DGNULIB_LIBPOSIX"
   CFLAGS="-I$INSTALL_PATH/include -DGNUTLS_INTERNAL_BUILD=1 -DCARES_STATICLIB=1 -DPCRE2_STATIC=1 -DNDEBUG $CFLAGS -flto=$(nproc) -DF_DUPFD=0 -DF_GETFD=1 -DF_SETFD=2" \
   LDFLAGS="-L$INSTALL_PATH/lib -static -static-libgcc $LDFLAGS" \
   GNUTLS_CFLAGS=$CFLAGS \
@@ -483,7 +483,7 @@ else
   rm -rf wget-*
   wget -O- https://ftp.gnu.org/gnu/wget/wget-1.25.0.tar.gz | tar xz
   cd wget-* || exit 1
-  sed -i '/#include <config.h>/a #include <stdlib.h>' lib/openat-die.c
+  export CPPFLAGS="$CPPFLAGS -DGNULIB_LIBPOSIX"
   chmod +x configure
   # cp ../windows-openssl.diff .
   # patch src/openssl.c < windows-openssl.diff
