18907f0b1832e40091497dd57772e64971e083b0
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2026-05-11 12:22:57 +0800

    Update wget-build.sh

--- wget-build.sh
+++ wget-build.sh
@@ -280,7 +280,7 @@ build_expat() {
   echo "⭐⭐⭐⭐⭐⭐$(date '+%Y/%m/%d %a %H:%M:%S.%N') - build expat⭐⭐⭐⭐⭐⭐"
   (
     if [ ! -f "$INSTALL_PATH"/lib/libexpat.a ]; then
-      wget -q -O- https://github.com/libexpat/libexpat/releases/download/R_2_8_0/expat-2.8.0.tar.gz | tar xz
+      wget -q -O- https://github.com/libexpat/libexpat/releases/download/R_2_8_1/expat-2.8.1.tar.gz | tar xz
       cd expat-* || exit
       LDFLAGS="$LDFLAGS_DEPS" ./configure --host=$WGET_MINGW_HOST --disable-shared --prefix="$INSTALL_PATH" --enable-static --without-docbook --without-tests
       make -j$(nproc) && make install
