4bbe8dbe4a1b18e5c14af903e98ffcb3bf751810
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2026-06-27 22:55:32 +0800

    Update build_wget_deps.sh

--- build_wget_deps.sh
+++ build_wget_deps.sh
@@ -246,6 +246,7 @@ build_gpgme() {
   tar xf "$tarball" || exit 1
   rm -f "$tarball"
   cd gpgme-* || exit 1
+  sed -i '/gpg_err_set_errno (EIO);/{n;s/^#else$/#endif/;}' src/w32-io.c
   env PYTHON="$(command -v python3 || command -v python)" LDFLAGS="$LDFLAGS_DEPS" \
   ./configure --host=$WGET_MINGW_HOST --disable-shared --prefix="$INSTALL_PATH" --enable-static \
     --with-libgpg-error-prefix="$INSTALL_PATH" --disable-gpg-test --disable-g13-test \
